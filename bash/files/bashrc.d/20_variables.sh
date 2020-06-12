69ba8b27dec766ea7eff31bba65346c6df09c596
Fabien Malfoy <fabien.malfoy@laposte.net>
2020-06-12 14:40:17 +0200

    [bash] Move variables to .bashrc.d/20_variables.sh

---
+++ bash/files/bashrc.d/20_variables.sh
@@ -0,0 +1,8 @@
+#------------------------------------------------------------------------------#
+#-- Variables
+#------------------------------------------------------------------------------#
+export LANG=en_US.utf8
+export EDITOR=vim
+export LESS=FRXS
+export HISTCONTROL=ignoredups:ignorespace
+export PATH=$PATH:$HOME/.local/bin
