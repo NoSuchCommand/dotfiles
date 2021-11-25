e0a34a6238c878badf90f85965b250af00d5d4b3
Fabien Malfoy <fabien.malfoy@laposte.net>
2021-11-25 21:33:35 +0100

    [bash] Add aliases

--- bash/files/bashrc.d/00_aliases.sh
+++ bash/files/bashrc.d/00_aliases.sh
@@ -6,3 +6,7 @@ alias evince=xreader
 alias gw='git what'
 alias view='vim -R'
 alias unvv=deactivate
+alias groot='cd $(git root)'
+alias issh='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root'
+alias iscp='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+alias pissh='pssh -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null"'
