6256f1b83f8c0560d8001f2bff6a75f18b8161a4
Fabien Malfoy <fabien.malfoy@laposte.net>
2022-12-09 10:13:23 +0100

    [bash] Connect as root with iscp

--- bash/files/bashrc.d/00_aliases.sh
+++ bash/files/bashrc.d/00_aliases.sh
@@ -8,5 +8,5 @@ alias view='vim -R'
 alias unvv=deactivate
 alias groot='cd $(git root)'
 alias issh='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root'
-alias iscp='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+alias iscp='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o User=root'
 alias pissh='pssh -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null"'
